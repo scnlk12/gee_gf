f55dc44268b9672e86524353c05d637f7e489091
fan <1206203790@qq.com>
2026-01-06 21:08:50 +0800

    feat: gee框架调用测试

---
+++ base6_router_group/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(ctx *gee.Context) {
+			// expect /hello?name=geektutu
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			// expect /hello/geektutu
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
+		})
+		v2.POST("/login", func (ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+	}
+
+	r.Run(":9999")
+}
