aa1a647938e19431db35f023e31daeb1801bafb6
fan <1206203790@qq.com>
2026-01-06 20:56:06 +0800

    chore: 定义RouterGroup实现路由的分组控制

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,29 +10,54 @@ type HandleFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
+	*RouterGroup // 将Engine作为最顶层的分组，Engine拥有RouterGroup所有的能力
 	router *router
+	groups []*RouterGroup // store all groups
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandleFunc // support middleware
+	parent      *RouterGroup // support nesting
+	engine      *Engine      // all groups share a Engine instance
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandleFunc)  {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandleFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandleFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandleFunc)  {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandleFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
 }
