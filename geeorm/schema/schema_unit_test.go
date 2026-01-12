57a1bbe98e526bef70a33db191d2b150da43e6d0
fan <1206203790@qq.com>
2026-01-12 18:21:06 +0800

    feat: 验证schema中的parse函数

---
+++ geeorm/schema/schema_unit_test.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"geeorm/dialect"
+	"testing"
+)
+
+type User struct {
+	Name string `geeorm:"primary key"`
+	Age  int
+}
+
+var TestDial, _ = dialect.GetDialect("sqlite3")
+
+func TestParse(t *testing.T) {
+	schema := Parse(&User{}, TestDial)
+	if schema.Name != "User" || len(schema.Fields) != 2 {
+		t.Fatal("failed to parse User struct")
+	}
+	if schema.GetField("Name").Tag != "primary key" {
+		t.Fatal("failed to parse primary key")
+	}
+}
