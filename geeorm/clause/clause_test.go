85c3f94d3ff070d61682b2ac3aba301c223a17c7
fan <1206203790@qq.com>
2026-01-13 14:51:14 +0800

    feat: 新增generator.go, clause.go实现sql语句构造

---
+++ geeorm/clause/clause_test.go
@@ -0,0 +1,28 @@
+package clause
+
+import (
+	"reflect"
+	"testing"
+)
+
+func testSelect(t *testing.T) {
+	var clause Clause
+	clause.Set(LIMIT, 3)
+	clause.Set(SELECT, "User", []string{"*"})
+	clause.Set(WHERE, "Name = ?", "Tom")
+	clause.Set(ORDERBY, "Age ASC")
+	sql, vars := clause.Build(SELECT, WHERE, ORDERBY, LIMIT)
+	t.Log(sql, vars)
+	if sql != "select * from User where Name = ? order by Age ASC limit ?" {
+		t.Fatal("failed to build sql")
+	}
+	if !reflect.DeepEqual(vars, []Interface{}{"Tom", 3}) {
+		t.Fatal("failed to build SQLVars")
+	}
+}
+
+func TestClause_Build(t *testing.T) {
+	t.Run("select", func(t *testing.T) {
+		testSelect(t)
+	})
+}
---
+++ geeorm/clause/generator.go
@@ -0,0 +1,77 @@
+package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+	generators[VALUES] = _values
+	generators[SELECT] = _select
+	generators[LIMIT] = _limit
+	generators[WHERE] = _where
+	generators[ORDERBY] = _orderBy
+}
+
+func genBinVars(num int) string {
+	var vars []string
+	for i := 0; i < num; i++ {
+		vars = append(vars, "?")
+	}
+	return strings.Join(vars, ", ")
+}
+
+func _insert(values ...interface{}) (string, []interface{}) {
+	// insert into $tableName ($fields)
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("insert into %s (%v)", tableName, fields), []interface{}{}
+}
+
+func _values(values ...interface{}) (string, []interface{}) {
+	// VALUES ($v1), ($v2), ...
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("values")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			bindStr = genBinVars(len(v))
+		}
+		sql.WriteString(fmt.Sprintf("(%v)", bindStr))
+		if i+1 != len(values) {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+}
+
+func _select(values ...interface{}) (string, []interface{}) {
+	// select $field from $tableName
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("select %v from %s", fields, tableName), []interface{}{}
+}
+
+func _limit(values ...interface{}) (string, []interface{}) {
+	// limit %num
+	return "limit ?", values
+}
+
+func _where(values ...interface{}) (string, []interface{}) {
+	// where $desc
+	desc, vars := values[0], values[1:]
+	return fmt.Sprintf("where %s", desc), vars
+}
+
+func _orderBy(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("order by %s", values[0]), []interface{}{}
+}
