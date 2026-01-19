c0825222e016c5b06128ff59d5a574f116d1b920
fan <1206203790@qq.com>
2026-01-19 21:01:04 +0800

    feat: 测试在服务端实现服务注册与调用方法

---
+++ geerpc/unit_server_test.go
@@ -0,0 +1,50 @@
+package geerpc
+
+import (
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func startServer(addr chan string) {
+	var foo Foo
+	if err := Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	Accept(l)
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := Dial("tcp", <-addr)
+	defer func() {
+		_ = client.Close()
+	}()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
