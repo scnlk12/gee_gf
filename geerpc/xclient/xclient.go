3a9d2a7d2501c675ba11eb5be09ebe20f5c5f16d
fan <1206203790@qq.com>
2026-01-21 12:45:14 +0800

    feat: 实现客户端的Call方法

---
+++ geerpc/xclient/xclient.go
@@ -0,0 +1,73 @@
+package xclient
+
+import (
+	"context"
+	"geerpc"
+	"io"
+	"sync"
+)
+
+// 支持负载均衡的客户端
+type XClient struct {
+	d    Discovery
+	mode SelectMode
+	opt  *geerpc.Option
+	mu   sync.Mutex // protect following
+	// 为了尽量复用已经创建好的Socket连接，使用clients保存创建成功的Client实例
+	clients map[string]*geerpc.Client
+}
+
+var _ io.Closer = (*XClient)(nil)
+
+func NewXClient(d Discovery, mode SelectMode, opt *geerpc.Option) *XClient {
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*geerpc.Client)}
+}
+
+func (xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	for key, client := range xc.clients {
+		_ = client.Close()
+		delete(xc.clients, key)
+	}
+	return nil
+}
+
+func (xc *XClient) dial(rpcAddr string) (*geerpc.Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	client, ok := xc.clients[rpcAddr]
+	if ok && !client.IsAvailable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	if client == nil {
+		var err error
+		client, err = geerpc.XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+
+// Call invokes the named function, waits for it to complete.
+// and returns its error status.
+// xc will choose a proper server.
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
